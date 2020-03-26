3ac5dee4c994942968a9d2f65542e65bd3d12eff
TheSteamingArtist <50603055+TheSteamingArtist@users.noreply.github.com>
2020-03-25 22:32:04 -0400

    Game Submission(Please Fix)

---
+++ Grappling Hook Game/Assets/Hookshot/Scripts/OnTriggerLoadLevel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerLoadLevel : MonoBehaviour
+{
+    public GameObject guiObject;
+    public string levelToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        guiObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay (Collider Other)
+    {
+        if (Other.gameObject.tag == "Player")
+        {
+            guiObject.SetActive(true);
+            if (guiObject.activeInHierarchy == true && Input.GetButtonDown("Use"))
+            {
+                Application.LoadLevel(levelToLoad);
+            }
+        }
+    }
+     void OnTriggerExit()
+    {
+        guiObject.SetActive(false);
+    }
+}
---
+++ Grappling Hook Game/Assets/Hookshot/Scripts/VolumeSetterLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeSetterLevel : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.volume = PlayerPrefs.GetFloat("MusiVolu", audioSource.volume);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
