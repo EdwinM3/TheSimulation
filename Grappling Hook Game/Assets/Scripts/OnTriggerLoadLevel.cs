329ccd9800656d4c6bb66e3ae4cbfef4a1794543
TheSteamingArtist <50603055+TheSteamingArtist@users.noreply.github.com>
2020-04-07 22:49:40 -0400

    Changes done to levels

--- Grappling Hook Game/Assets/Scripts/OnTriggerLoadLevel.cs
+++ Grappling Hook Game/Assets/Scripts/OnTriggerLoadLevel.cs
@@ -1,15 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OnTriggerLoadLevel : MonoBehaviour
-{
-    public GameObject guiObject;
-    public string levelToLoad;
+{ 
     // Start is called before the first frame update
     void Start()
     {
-        guiObject.SetActive(false);
+        
     }
 
     // Update is called once per frame
@@ -17,15 +16,7 @@ public class OnTriggerLoadLevel : MonoBehaviour
     {
         if (Other.gameObject.tag == "Player")
         {
-            guiObject.SetActive(true);
-            if (guiObject.activeInHierarchy == true && Input.GetButtonDown("Use"))
-            {
-                Application.LoadLevel(levelToLoad);
-            }
+            SceneManager.LoadScene(3);
         }
-    }
-     void OnTriggerExit()
-    {
-        guiObject.SetActive(false);
     }
 }
