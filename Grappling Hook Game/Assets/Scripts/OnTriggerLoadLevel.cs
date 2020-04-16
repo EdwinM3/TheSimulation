96dd6e89f825d8b86c6cf65291738a0f858ca959
TheSteamingArtist <50603055+TheSteamingArtist@users.noreply.github.com>
2020-04-15 21:28:11 -0400

    Created a timer and made some changes to the level in terms of fixing bugs

--- Grappling Hook Game/Assets/Scripts/OnTriggerLoadLevel.cs
+++ Grappling Hook Game/Assets/Scripts/OnTriggerLoadLevel.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
+
 public class OnTriggerLoadLevel : MonoBehaviour
-{ 
+{
+
+   
+   
     // Start is called before the first frame update
     void Start()
+    {
+        
+    }
+
+    void Update()
     {
         
     }
@@ -16,7 +27,30 @@ public class OnTriggerLoadLevel : MonoBehaviour
     {
         if (Other.gameObject.tag == "Player")
         {
+            SceneChecker();
+            
+               
+           
+        }
+    }
+
+
+    void SceneChecker()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        string sceneName = currentScene.name;
+
+        int buildIndex = currentScene.buildIndex;
+
+        if(buildIndex == 1)
+        {
+            SceneManager.LoadScene(2);
+        }
+        if(buildIndex == 2)
+        {
             SceneManager.LoadScene(3);
         }
+
     }
 }
---
+++ Grappling Hook Game/Assets/Stephens_Assets/Stephens_Scripts/KillBoxScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KillBoxScript : MonoBehaviour
+{
+    public Vector3 respawnPosition;
+
+    public TextMeshProUGUI warningTextmeshPro;
+
+    private IEnumerator coroutine;
+
+    private int AmountofTime = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   void OnControllerColliderHit(ControllerColliderHit hit)
+    {       
+        if (hit.gameObject.tag == "Killbox")
+        {
+            StartCoroutine("OutofBounds", true);
+
+        }
+        else
+        {
+            AmountofTime = 500;
+            StopCoroutine("OutofBounds");
+        }
+    } 
+    
+    public IEnumerator OutofBounds()
+    {
+        
+        for (int i = AmountofTime; i >= 0; i--)
+        {
+            yield return new WaitForSeconds(2);
+            
+            AmountofTime--;
+            warningTextmeshPro.text = "Warning ! Please turn back! :" + AmountofTime.ToString();
+
+            yield return new WaitForSeconds(.5f);
+            warningTextmeshPro.text = "";
+            yield return new WaitForSeconds(.5f);
+            warningTextmeshPro.text = "Warning ! Please turn back! :" + AmountofTime.ToString();
+            yield return new WaitForSeconds(.5f);
+
+            if (AmountofTime == 0)
+            {
+                transform.position = respawnPosition;
+            }
+        }
+    }
+}
