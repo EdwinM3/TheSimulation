7ecfa78dd132f2881b3b084fe50236a5f0336b65
TheSteamingArtist <50603055+TheSteamingArtist@users.noreply.github.com>
2020-05-03 22:53:53 -0400

    EndingScene

---
+++ Grappling Hook Game/Assets/Stephens_Assets/Stephens_Scripts/LoadTitle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadTitle : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
