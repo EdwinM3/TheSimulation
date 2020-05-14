d04fcd9c900678c6cd91ce4cf284514e7b5e2fac
TheSteamingArtist <50603055+TheSteamingArtist@users.noreply.github.com>
2020-05-13 21:57:26 -0400

    a

---
+++ Grappling Hook Game/Assets/Stephens_Assets/Stephens_Scripts/JumpingSoundScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingSoundScript : MonoBehaviour
+{
+
+    public static AudioSource audio;
+    public static AudioClip jumpSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            audio.PlayOneShot(jumpSound);
+        }
+        
+             
+    }
+}
+
